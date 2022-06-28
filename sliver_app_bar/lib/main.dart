8611ca15e692d1cf422ceda0973527ddcc3cb69a
M.Zeynep <95376085+mzyavuz@users.noreply.github.com>
2022-06-28 12:50:08 +0300

    ilk dosyalar eklendi

---
+++ sliver_app_bar/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Anasayfa(),
+    );
+  }
+}
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({Key? key}) : super(key: key);
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(),
+        ],
+      ),
+    );
+  }
+}
